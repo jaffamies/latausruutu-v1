32510cd4c8d442cb3b2dd6ec5839d19b4014ddfe
jaffamies <46383800+jaffamies@users.noreply.github.com>
2020-11-28 21:49:15 +0200

    Add files via upload

--- client.lua
+++ client.lua
@@ -1,12 +1,9 @@
 local Ran = false
 
--- Wait until client is loaded into the map
 AddEventHandler("playerSpawned", function ()
-	-- If not already ran
 	if not Ran then
-		-- Close loading screen resource
+		-- Lopeta latausruutu
 		ShutdownLoadingScreenNui()
-		-- Set as ran
 		Ran = true
 	end
 end)
